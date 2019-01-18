53b9a0e796d00e493820a4e3197ae0c33286158a
jds-walker <jds_walker@optusnet.com.au>
2019-01-18 13:18:22 +1100

    playing with timeout / memsize

--- handler.py
+++ handler.py
@@ -1,10 +1,12 @@
 import json
+import time
  
 
 def hello(event, context):
     print('first second update')
+    time.sleep(4)
     print('hello world')
-
+    
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
